b622eddcd81e83385bac5820d9b4f6dbf8055229
Daiki Matsudate <d.matsudate@gmail.com>
2020-03-11 21:35:30 +0900

    update examples
    update examples

--- Examples/generate.sh
+++ Examples/generate.sh
@@ -1,19 +1,20 @@
-rm -rf ./Petstore
+rm -rf ./PetStore
 
 openapi-generator generate \
 -t swift \
 -i https://petstore.swagger.io/v2/swagger.json \
 -g swift4 \
 -c swift.json \
--o ./Petstore
+-o ./PetStore
 
-if type "xcodegen" > /dev/null 2>&1; then
-  cd Petstore
-  xcodegen generate
-  if type "carthage" > /dev/null 2>&1; then
-    carthage update --no-use-binaries --platform ios --cache-builds
-  fi
-fi
+mkdir ./PetStore/Sources
+mkdir ./PetStore/Sources/PetStore
+mv ./Petstore/PetStore/Classes/OpenAPIs/APIs ./PetStore/Sources/PetStore/APIs
+mv ./Petstore/PetStore/Classes/OpenAPIs/Models ./PetStore/Sources/PetStore/Models
+mv ./Petstore/PetStore/Classes/OpenAPIs/APIs.swift ./PetStore/Sources/PetStore/APIs.swift
+rm -rf ./Petstore/PetStore
+
+swift package generate-xcodeproj
 
 if type "swiftlint" > /dev/null 2>&1; then
   cd Petstore
